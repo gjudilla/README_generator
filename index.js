97b870f87636a54199f163381ae590976801e823
gjudilla <gjudilla@gmail.com>
2024-02-13 22:21:16 -0800

    README template

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// const generateREADME = ({ title, description, table_of_contents, installation, usage, license, contributing, tests, and questions }) =>
+// `#${title}
+
+// ## Description
+// #${description}
+
+// ## Table of Contents
+// #${table_of_contents}
+
+// ## Installation
+// #${installation}
+
+// ## Usage
+// #${usage}
+
+// ## License
+// #${license}
+
+// ## Contributing
+// #${contributing}
+
+// ## Tests
+// #${tests}
+
+// ## Questions
+// #${questions}
+
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = []; 
+
+// return inquirer.prompt ([
+//     {}
+// ])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
